4678a6ad80b6961e50977913498c305f77d995e1
Jakub Mitoraj MITOsoft <dxtr85@gmail.com>
2017-10-25 17:40:04 +0200

    Added gbl alias and gbn  function to list and select branch.

--- install_aliases.sh
+++ install_aliases.sh
@@ -1,4 +1,6 @@
 echo "alias ser=\"grep -rnw ./ -e\"" >> ~/.bashrc
 echo "alias serpy=\"grep --include=\*.py -rnw ./ -e\"" >> ~/.bashrc
+echo "alias gbl=\"git branch| awk '{printf(\\\"%3d: %s\n\\\", NR, \\\$0)}'\"" >> ~/.bashrc
+echo "gbn() { if [[ \$# -ne 1 ]] ; then echo \"You need to pass a number argument!\" ; else eval \"git branch | awk 'NR == \$1'\" ; fi }"  >> ~/.bashrc
 
 source ~/.bashrc
